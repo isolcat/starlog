1bcc601553f372559bbf3f5a1ca97305fec1ad4a
isolcat <308750414@qq.com>
2026-01-01 19:37:58 +0800

    feat: add multiple blog posts and update site metadata

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'starlog!',
+  title: 'starlog',
   author: 'Stone',
-  headerTitle: 'starlog!',
+  headerTitle: 'starlog',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
